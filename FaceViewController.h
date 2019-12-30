6b28ca218403c333deb52b3ba452b29ca6979960
yr1216 <15313971471@163.com>
2019-12-30 11:01:31 +0800

    Create FaceViewController.h

---
+++ FaceViewController.h
@@ -0,0 +1,17 @@
+//
+//  FaceViewController.h
+//  Demo02
+//
+//  Created by sinosoft on 2019/12/27.
+//  Copyright © 2019 sinosoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FaceViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
